8bd1dd0e19f4190ec69d9f8bf34004abb6d3d8cc
Carlos Balcazar <carlosbmtz@gmail.com>
2018-03-27 16:38:11 -0600

    Initial commit

---
+++ Proyecto de dispositivos 2,0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Proyecto de dispositivos 2,0
+//
+//  Created by Carlos Balcazar on 27/03/18.
+//  Copyright © 2018 The Way 2.0. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
