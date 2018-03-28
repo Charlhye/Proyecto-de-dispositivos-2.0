67cf0a8d18a8a2a519b10cebd1e1baffcbf60846
Carlos Balcazar <carlosbmtz@gmail.com>
2018-03-28 16:39:17 -0600

    Permiso de camara

--- Proyecto de dispositivos 2,0/TableViewController2.swift
+++ Proyecto de dispositivos 2,0/TableViewController2.swift
@@ -1,5 +1,5 @@
 //
-//  TableViewController2.swift
+//  TableViewController1TableViewController.swift
 //  Proyecto de dispositivos 2,0
 //
 //  Created by Manuel Avalos Tovar on 3/28/18.
@@ -20,12 +20,7 @@ class TableViewController2: UITableViewController {
     
     //paso 8: agregar la direccion de donde se recuperan los datos en linea
     let direccion="http://199.233.252.86/datos/datos.json"
-    private let datos = [
-        "Ford", "BMW", "Audi", "VW", "Chrysler", "Nissan", "Peugeot", "Renault", "Seat", "Citroen"
-    ]
     
-    //paso 1: agregar nuevos datos en formato JSON
-    let datosJSON = "[ {\"Marca\": \"FORD\", \"Agencias\": 21}, {\"marca\": \"BMW\", \"agencias\": 35} ]"
     //paso 2: declarar una variable para contener los nuevos datos
     var nuevoArray:[Any]?
     
@@ -51,16 +46,9 @@ class TableViewController2: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-        
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-        //nuevoArray = JSONParseArray(datosJSON)
         let url = URL(string: direccion)
         let datos = try? Data(contentsOf: url!)
         
-        //paso 4: llenar el nuevo arreglo
         nuevoArray = try! JSONSerialization.jsonObject(with: datos!) as? [Any]
     }
     
@@ -98,63 +86,12 @@ class TableViewController2: UITableViewController {
         cell.textLabel?.text=s
         return cell
         
-        // Configure the cell...
-        
-        /*
-         let cell = tableView.dequeueReusableCell(withIdentifier: "entradaMarca", for: indexPath)
-         
-         // Configure the cell...
-         //cell.textLabel?.text = datos[indexPath.row]
-         //paso 6: reemplazar el uso de datos por nuevo array
-         //objeto marca es un diccionario que contiene marca y agencias
-         let objetoMarca = nuevoArray?[indexPath.row] as! [String: Any]
-         let s:String = objetoMarca["Marca"] as! String
-         
-         cell.textLabel?.text=s
-         
-         return cell
-         */
     }
     
     
-    /*
-     // Override to support conditional editing of the table view.
-     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
-    
-    /*
-     // Override to support editing the table view.
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-     if editingStyle == .delete {
-     // Delete the row from the data source
-     tableView.deleteRows(at: [indexPath], with: .fade)
-     } else if editingStyle == .insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
-    
-    /*
-     // Override to support rearranging the table view.
-     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-     
-     }
-     */
-    
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
-    
     
     // MARK: - Navigation
-    
+    /*
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
@@ -167,8 +104,7 @@ class TableViewController2: UITableViewController {
         let s:String = objetoMarca["marca"] as! String
         
         sigVista.valorRecibido = s
-    }
+    }*/
     
     
-}
-
+}//
