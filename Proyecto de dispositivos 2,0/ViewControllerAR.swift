90b0d92731ba1468cc8be784847f3b5632c1277a
Carlos Balcazar <carlosbmtz@gmail.com>
2018-03-28 18:51:51 -0600

    ARKit with static model

--- Proyecto de dispositivos 2,0/ViewControllerAR.swift
+++ Proyecto de dispositivos 2,0/ViewControllerAR.swift
@@ -12,69 +12,73 @@ import ARKit
 
 class ViewControllerAR: UIViewController, ARSCNViewDelegate {
 
-    @IBOutlet var sceneView: ARSCNView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Set the view's delegate
-        sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = false
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        // Create a session configuration
+        //etiqueta para indicar al usuario que el plano horizontal ha sido detectadi
+        @IBOutlet weak var planeDetected: UILabel!
+        //variable para manejar la escena
+        @IBOutlet weak var sceneView: ARSCNView!
         let configuration = ARWorldTrackingConfiguration()
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            //mostrar el origen y los puntos detectados
+            self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+            //indicar la detección del plano
+            self.configuration.planeDetection = .horizontal
+            self.sceneView.session.run(configuration)
+            self.sceneView.delegate = self
+            //administrador de gestos para identificar el tap sobre el plano horizontal
+            let tap = UITapGestureRecognizer(target: self, action: #selector(tapHandler))
+            self.sceneView.addGestureRecognizer(tap)
+        }
+        //función administradora de gestos
+        @objc func tapHandler(sender: UITapGestureRecognizer){
+            guard let sceneView = sender.view as? ARSCNView else {return}
+            let touchLocation = sender.location(in: sceneView)
+            //obtener los resultados del tap sobre el plano horizontal
+            let hitTestResult = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
+            if !hitTestResult.isEmpty{
+                //cargar la escena
+                self.addPortal(hitTestResult: hitTestResult.first!)
+            }
+            else{
+                // no hubo resultado
+            }
+            
+            
+        }
+        //cargar el portal
+        func addPortal(hitTestResult:ARHitTestResult)
+        {
+            let portalScene = SCNScene(named:"escenes.sncassets/Portal.scn")
+            let portalNode = portalScene?.rootNode.childNode(withName: "Portal", recursively: false)
+            //convertir las coordenadas del rayo del tap a coordenadas del mundo real
+            let transform = hitTestResult.worldTransform
+            let planeXposition = transform.columns.3.x
+            let planeYposition = transform.columns.3.y
+            let planeZposition = transform.columns.3.z
+            portalNode?.position = SCNVector3(planeXposition,planeYposition,planeZposition)
+            self.sceneView.scene.rootNode.addChildNode(portalNode!)
+            
+        }
+        
+        override func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
+            // Dispose of any resources that can be recreated.
+        }
+        //esta funcion indica al delegado que se ha agregado un nuevo nodo en la escena
+        /* para mayor detalle https://developer.apple.com/documentation/arkit/arscnview/providing_3d_virtual_content_with_scenekit
+         */
+        func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+            guard anchor is ARPlaneAnchor else {return} //se agrego un plano
+            //ejecución asincrona en donde se modifica la etiqueta de plano detectado
+            DispatchQueue.main.async {
+                self.planeDetected.isHidden = false
+                print("Plano detectado")
+            }
+            //espera 3 segundos antes de desaparecer
+            DispatchQueue.main.asyncAfter(deadline: .now()+3){self.planeDetected.isHidden = true}
+        }
+        
+    
 
-        // Run the view's session
-        sceneView.session.run(configuration)
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        
-        // Pause the view's session
-        sceneView.session.pause()
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
 }
